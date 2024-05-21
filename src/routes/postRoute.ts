052c8c4b671fe605df81824d5e8823c14345b566
DatLe3009 <105985045+DatLe3009@users.noreply.github.com>
2024-05-22 05:10:23 +0700

    adjust postRoute

--- src/routes/postRoute.ts
+++ src/routes/postRoute.ts
@@ -6,20 +6,20 @@ import { userRole } from "../utils/enum";
 const route = express.Router()
 
 route.get('/job-postings', JobPostingController.getAllJobPostings);
-route.get('/api/v1/job-postings/totalResults', JobPostingController.getLengthOfAllJobPostings);
-route.get('/api/v1/job-postings/totalResultsOfProfession', JobPostingController.getTotalResultsOfProfession);
-route.get('/api/v1/admin/job-postings/totalResultsOfProfession', verifyToken, verifyRole(userRole.Admin), JobPostingController.getTotalResultsOfProfessionByAdmin);
-route.get('/api/v1/admin/job-postings', verifyToken, verifyRole(userRole.Admin), JobPostingController.getAllJobPostingsByAdmin);
-route.get('/api/v1/admin/job-postings/totalResults', verifyToken, verifyRole(userRole.Admin), JobPostingController.getLengthOfAllJobPostingsByAdmin);
-route.get('/api/v1/job-postings/:postId', JobPostingController.getJobPosting);
+route.get('/job-postings/totalResults', JobPostingController.getLengthOfAllJobPostings);
+route.get('/job-postings/totalResultsOfProfession', JobPostingController.getTotalResultsOfProfession);
+route.get('/admin/job-postings/totalResultsOfProfession', verifyToken, verifyRole(userRole.Admin), JobPostingController.getTotalResultsOfProfessionByAdmin);
+route.get('/admin/job-postings', verifyToken, verifyRole(userRole.Admin), JobPostingController.getAllJobPostingsByAdmin);
+route.get('/admin/job-postings/totalResults', verifyToken, verifyRole(userRole.Admin), JobPostingController.getLengthOfAllJobPostingsByAdmin);
+route.get('/job-postings/:postId', JobPostingController.getJobPosting);
 
-route.get('/api/v1/employer/job-postings', verifyToken, verifyRole(userRole.Employer), JobPostingController.getJobPostingsByEmployer);
-route.get('/api/v1/employer/job-postings/:postId', verifyToken, verifyRole(userRole.Employer), JobPostingController.getJobPostingByEmployer);
-route.post('/api/v1/employer/job-postings', verifyToken, verifyRole(userRole.Employer), JobPostingController.createNewJobPosting);
-route.put('/api/v1/employer/job-postings/:postId', verifyToken, verifyRole(userRole.Employer), JobPostingController.updateJobPosting);
-route.delete('/api/v1/employer/job-postings/:postId', verifyToken, verifyRole(userRole.Employer), JobPostingController.deleteJobPosting);
+route.get('/employer/job-postings', verifyToken, verifyRole(userRole.Employer), JobPostingController.getJobPostingsByEmployer);
+route.get('/employer/job-postings/:postId', verifyToken, verifyRole(userRole.Employer), JobPostingController.getJobPostingByEmployer);
+route.post('/employer/job-postings', verifyToken, verifyRole(userRole.Employer), JobPostingController.createNewJobPosting);
+route.put('/employer/job-postings/:postId', verifyToken, verifyRole(userRole.Employer), JobPostingController.updateJobPosting);
+route.delete('/employer/job-postings/:postId', verifyToken, verifyRole(userRole.Employer), JobPostingController.deleteJobPosting);
 
-route.put('/api/v1/admin/job-postings/:postId', verifyToken, verifyRole(userRole.Admin), JobPostingController.updateApprovalStatus);
+route.put('/admin/job-postings/:postId', verifyToken, verifyRole(userRole.Admin), JobPostingController.updateApprovalStatus);
 
 
 
