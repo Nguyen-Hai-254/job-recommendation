7788211d26862784327fd5fd01a08d6ba3ef51f3
DatLe3009 <105985045+DatLe3009@users.noreply.github.com>
2024-06-03 01:05:15 +0700

    cors: allows postman, ....

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ connectDB();
 // 2.1. cors
 var corsOptions = {
     origin: function (origin, callback) {
-      if (origin == process.env.ORIGIN || process.env.ORIGIN == '*') {
+      if (origin == process.env.ORIGIN || process.env.ORIGIN == '*' || !origin) {
         callback(null, true)
       } else {
         callback(new Error('Not allowed by CORS'))
